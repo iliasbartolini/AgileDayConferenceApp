6a460cf71c55e54503b0ce79698cd793ba06985b
Ilias Bartolini <ilias.bartolini@gmail.com>
2010-09-25 01:37:05 +0200

    Aggiunta Activity Session

---
+++ AgileDayConferenceApp/src/it/agileday/SessionActivity.java
@@ -0,0 +1,13 @@
+package it.agileday;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SessionActivity extends Activity {
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.main);
+       }
+	        
+
+}
