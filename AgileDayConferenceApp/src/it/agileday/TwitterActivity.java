a648d5999f26d4fd2ac407e6642761600cf14be2
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-09-26 12:05:27 +0200

    Twitter activity: Adde basic list-filling

--- AgileDayConferenceApp/src/it/agileday/TwitterActivity.java
+++ AgileDayConferenceApp/src/it/agileday/TwitterActivity.java
@@ -1,11 +1,20 @@
 package it.agileday;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class TwitterActivity extends Activity {
+public class TwitterActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.twitter);
+		setContentView(R.layout.activity_twitter);
+		fillData();
+	}
+
+	private void fillData() {
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item_twitter);
+		adapter.add("Tweet 1");
+		adapter.add("Tweet 2");
+		setListAdapter(adapter);
 	}
 }
