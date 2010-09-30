b5d2ef9c2488041e6b90caa7f682ee5ad45e77ea
Ilias Bartolini <ilias.bartolini@gmail.com>
2010-09-30 03:20:31 +0200

    Fixed licence headers

--- AgileDayConferenceApp/src/it/agileday/ui/sessions/SessionActivity.java
+++ AgileDayConferenceApp/src/it/agileday/ui/sessions/SessionActivity.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2010 Ilias Bartolini
+   Copyright 2010 Ilias Bartolini, Luigi Bozzo, Andrea Chiarini
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
