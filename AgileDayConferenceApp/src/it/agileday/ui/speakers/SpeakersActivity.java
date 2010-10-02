acf42f46d0191a5a7a060cc68ce0190cd1c5e530
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-10-02 17:27:50 +0200

    Refactoring: Switched from SimpleCursorAdapter to custom CursorAdapter

--- AgileDayConferenceApp/src/it/agileday/ui/speakers/SpeakersActivity.java
+++ AgileDayConferenceApp/src/it/agileday/ui/speakers/SpeakersActivity.java
@@ -4,13 +4,16 @@ import it.agileday.R;
 import it.aglieday.data.Dao;
 import it.aglieday.data.DatabaseHelper;
 import android.app.Activity;
+import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
 import android.widget.Gallery;
-import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class SpeakersActivity extends Activity {
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,6 +24,27 @@ public class SpeakersActivity extends Activity {
 		startManagingCursor(speakers);
 
 		Gallery g = (Gallery) findViewById(R.id.gallery);
-		g.setAdapter(new SimpleCursorAdapter(this, R.layout.speakers_item, speakers, new String[] { "name", "bio" }, new int[] { R.id.name, R.id.bio }));
+		g.setAdapter(new SpeakersCursorAdapter(this, speakers));
+	}
+
+	private class SpeakersCursorAdapter extends CursorAdapter {
+		public SpeakersCursorAdapter(Context context, Cursor c) {
+			super(context, c);
+		}
+
+		@Override
+		public void bindView(View view, Context context, Cursor cursor) {
+			TextView name = (TextView) view.findViewById(R.id.name);
+			name.setText(cursor.getString(cursor.getColumnIndexOrThrow("name")));
+			TextView bio = (TextView) view.findViewById(R.id.bio);
+			bio.setText(cursor.getString(cursor.getColumnIndexOrThrow("bio")));
+		}
+
+		@Override
+		public View newView(Context context, Cursor cursor, ViewGroup parent) {
+			View ret = getLayoutInflater().inflate(R.layout.speakers_item, parent, false);
+			bindView(ret, context, cursor);
+			return ret;
+		}
 	}
 }
