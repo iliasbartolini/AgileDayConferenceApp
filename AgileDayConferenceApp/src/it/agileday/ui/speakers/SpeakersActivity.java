59f8377a7e02382b387329d514d1402948c94146
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-10-03 23:05:11 +0200

    Change Speakers activity implementation using simple ViewAnimator. Not beautiful as the previous implementation, but much more easy to implement

--- AgileDayConferenceApp/src/it/agileday/ui/speakers/SpeakersActivity.java
+++ AgileDayConferenceApp/src/it/agileday/ui/speakers/SpeakersActivity.java
@@ -7,19 +7,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Display;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
-import android.view.WindowManager;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
 import android.widget.ViewAnimator;
 
 public class SpeakersActivity extends Activity {
 	private ViewAnimator viewAnimator;
+	private GestureDetector gestureDetector;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,79 +25,41 @@ public class SpeakersActivity extends Activity {
 		Cursor speakers = dao.fetchAllSpeakers();
 		startManagingCursor(speakers);
 
-		// Gallery g = (Gallery) findViewById(R.id.gallery);
-		// g.setAdapter(new SpeakersCursorAdapter(this, speakers));
+		this.gestureDetector = new GestureDetector(this, new GestureListener());
 
 		viewAnimator = (ViewAnimator) findViewById(R.id.flipper);
-		Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-		viewAnimator.setOnTouchListener(new TouchListener(viewAnimator, display.getWidth()));
-		viewAnimator.setDisplayedChild(1);
-
+		viewAnimator.setDisplayedChild(0);
 	}
 
-	private static class TouchListener implements OnTouchListener {
-		private static final String TAG = TouchListener.class.getName();
-		private final ViewAnimator viewAnimator;
-		private float downXValue;
-		private final int displayWidth;
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return gestureDetector.onTouchEvent(event);
+	}
 
-		public TouchListener(ViewAnimator viewAnimator, int displayWidth) {
-			this.viewAnimator = viewAnimator;
-			this.displayWidth = displayWidth;
-		}
+	private Context getContext() {
+		return this;
+	}
 
+	private class GestureListener extends SimpleOnGestureListener {
 		@Override
-		public boolean onTouch(View v, MotionEvent event) {
-			final View leftView = viewAnimator.getChildAt(viewAnimator.getDisplayedChild() - 1);
-			final View centerView = viewAnimator.getCurrentView();
-			final View rightView = viewAnimator.getChildAt(viewAnimator.getDisplayedChild() + 1);
-			final float currentX = event.getX();
-			final boolean isRightDrag = downXValue < currentX;
-			final boolean isLeftDrag = downXValue > currentX;
-			final float xOffset = Math.abs(currentX - downXValue);
-			final float normalizedXOffset = xOffset / displayWidth;
-			final int action = event.getAction();
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			boolean flingRight = velocityX > 0;
+			boolean flingLeft = velocityX < 0;
+			int currentItem = viewAnimator.getDisplayedChild();
+			boolean hasNextItem = (viewAnimator.getChildCount() - 1 > currentItem);
+			boolean hasPrevItem = (currentItem > 0);
 
-			Log.d(TAG, "Touch action " + action + " on x=" + currentX + ", downXValue=" + downXValue);
-			if (action == MotionEvent.ACTION_DOWN) {
-				downXValue = event.getX();
-			} else if (action == MotionEvent.ACTION_MOVE) {
-				if (isLeftDrag && rightView != null) {
-					setViewXOffset(centerView, (int) -xOffset);
-					setViewXOffset(rightView, displayWidth - (int) xOffset);
-					rightView.setVisibility(View.VISIBLE);
-				} else if (isRightDrag && leftView != null) {
-					setViewXOffset(centerView, (int) xOffset);
-					setViewXOffset(leftView, (int) xOffset - displayWidth);
-					leftView.setVisibility(View.VISIBLE);
-				}
-			} else if (action == MotionEvent.ACTION_UP) {
-				if (isRightDrag && leftView != null) {
-					this.viewAnimator.setInAnimation(buildTranslateAnimation(normalizedXOffset - 1.0f, 0.0f));
-					this.viewAnimator.setOutAnimation(buildTranslateAnimation(0.0f, 1.0f - normalizedXOffset));
-					viewAnimator.showPrevious();
-				} else if (isLeftDrag && rightView != null) {
-					this.viewAnimator.setInAnimation(buildTranslateAnimation(1.0f - normalizedXOffset, 0.0f));
-					this.viewAnimator.setOutAnimation(buildTranslateAnimation(0.0f, normalizedXOffset - 1.0f));
-					viewAnimator.showNext();
-				}
+			if (flingRight && hasPrevItem) {
+				viewAnimator.setOutAnimation(getContext(), R.anim.slideout_toright);
+				viewAnimator.setInAnimation(getContext(), R.anim.slidein_fromleft);
+				viewAnimator.setDisplayedChild(currentItem - 1);
+			} else if (flingLeft && hasNextItem) {
+				viewAnimator.setOutAnimation(getContext(), R.anim.slideout_toleft);
+				viewAnimator.setInAnimation(getContext(), R.anim.slidein_fromright);
+				viewAnimator.setDisplayedChild(currentItem + 1);
 			}
+
 			return true;
-		}
-
-		private void setViewXOffset(View view, int offset) {
-			view.layout(offset, view.getTop(), view.getRight(), view.getBottom());
-		}
-
-		private TranslateAnimation buildTranslateAnimation(float fromXValue, float toXValue) {
-			TranslateAnimation ret = new TranslateAnimation(
-					Animation.RELATIVE_TO_PARENT, fromXValue,
-					Animation.RELATIVE_TO_PARENT, toXValue,
-					Animation.RELATIVE_TO_PARENT, 0.0f,
-					Animation.RELATIVE_TO_PARENT, 0.0f);
-			ret.setDuration(250);
-			ret.setInterpolator(new AccelerateDecelerateInterpolator());
-			return ret;
 		}
 	}
 
