cbd6e51e3c4bad486f845a81bb6028c9a86ee557
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-10-05 20:09:27 +0200

    Speaker image implemented using HTML tag. Don't like it much.

--- AgileDayConferenceApp/src/it/agileday/ui/speakers/SpeakersActivity.java
+++ AgileDayConferenceApp/src/it/agileday/ui/speakers/SpeakersActivity.java
@@ -7,11 +7,21 @@ import it.aglieday.data.SpeakerRepository;
 
 import java.util.List;
 
+import org.xml.sax.XMLReader;
+
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.Html;
+import android.text.Html.ImageGetter;
+import android.text.Html.TagHandler;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.ViewAnimator;
@@ -48,6 +58,21 @@ public class SpeakersActivity extends Activity {
 		bio.setFocusable(false);
 		bio.setFocusableInTouchMode(false);
 		bio.setLongClickable(false);
-		bio.setText(Html.fromHtml(speaker.bio));
+
+		bio.setText(Html.fromHtml(speaker.bio, new ImageGetter() {
+			@Override
+			public Drawable getDrawable(String source) {
+				int resourceId = getResources().getIdentifier(source, "drawable", "it.agileday");
+				Drawable drawable = getResources().getDrawable(resourceId);
+				drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+				return drawable;
+			}
+		}, null));
+
+//		Resources resources = context.getResources(); 
+//		Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + 
+//		resources.getResourcePackageName(resId) + '/' + 
+//		resources.getResourceTypeName(resId) + '/' + 
+//		resources.getResourceEntryName(resId) ); 
 	}
 }
