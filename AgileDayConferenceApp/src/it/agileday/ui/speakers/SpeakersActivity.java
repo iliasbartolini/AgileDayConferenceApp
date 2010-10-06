acfcf8b50950b1bf2fb75ed9f4e74a3011025dd7
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-10-06 08:43:11 +0200

    Other refinements on speakers gesture detecting

--- AgileDayConferenceApp/src/it/agileday/ui/speakers/SpeakersActivity.java
+++ AgileDayConferenceApp/src/it/agileday/ui/speakers/SpeakersActivity.java
@@ -8,10 +8,15 @@ import it.aglieday.data.SpeakerRepository;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,13 +25,49 @@ import android.widget.ViewAnimator;
 public class SpeakersActivity extends Activity {
 	private static final String TAG = SpeakersActivity.class.getName();
 	private ViewAnimator viewAnimator;
+	private GestureDetector gestureDetector;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.speakers);
 		viewAnimator = (ViewAnimator) findViewById(R.id.flipper);
+		gestureDetector = new GestureDetector(this, new GestureListener());
 		fillData();
+	}
+
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+		return gestureDetector.onTouchEvent(ev) || super.dispatchTouchEvent(ev);
+	}
+
+	private Context getContext() {
+		return this;
+	}
+
+	private class GestureListener extends SimpleOnGestureListener {
+		private final float FLING_THRESHOLD = 500.0f;
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+			Log.d(TAG, Float.valueOf(velocityX).toString());
+			boolean flingRight = velocityX > FLING_THRESHOLD;
+			boolean flingLeft = velocityX < -FLING_THRESHOLD;
+			int currentItem = viewAnimator.getDisplayedChild();
+			boolean hasNextItem = (viewAnimator.getChildCount() - 1 > currentItem);
+			boolean hasPrevItem = (currentItem > 0);
+
+			if (flingRight && hasPrevItem) {
+				viewAnimator.setOutAnimation(getContext(), R.anim.slideout_toright);
+				viewAnimator.setInAnimation(getContext(), R.anim.slidein_fromleft);
+				viewAnimator.setDisplayedChild(currentItem - 1);
+			} else if (flingLeft && hasNextItem) {
+				viewAnimator.setOutAnimation(getContext(), R.anim.slideout_toleft);
+				viewAnimator.setInAnimation(getContext(), R.anim.slidein_fromright);
+				viewAnimator.setDisplayedChild(currentItem + 1);
+			}
+			return true;
+		}
 	}
 
 	private void fillData() {
@@ -51,7 +92,7 @@ public class SpeakersActivity extends Activity {
 
 	private void setHtmlText(Speaker speaker, TextView bio) {
 		bio.setMovementMethod(LinkMovementMethod.getInstance()); // http://code.google.com/p/android/issues/detail?id=2219
-		bio.setClickable(false);
+		bio.setClickable(true);
 		bio.setFocusable(false);
 		bio.setFocusableInTouchMode(false);
 		bio.setLongClickable(false);
