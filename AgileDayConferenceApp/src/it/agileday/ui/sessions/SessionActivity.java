99daaa55b9fcc44ce66280968cc98d8aa56127cb
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-10-09 13:13:56 +0200

    Refactoring

--- AgileDayConferenceApp/src/it/agileday/ui/sessions/SessionActivity.java
+++ AgileDayConferenceApp/src/it/agileday/ui/sessions/SessionActivity.java
@@ -19,23 +19,17 @@ package it.agileday.ui.sessions;
 import it.agileday.R;
 import it.agileday.utils.GestureListener;
 import it.aglieday.data.DatabaseHelper;
-import it.aglieday.data.Session;
 import it.aglieday.data.Track;
 import it.aglieday.data.TrackRepository;
 
 import java.util.Collection;
-import java.util.List;
 
 import android.app.Activity;
-import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.GestureDetector;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ViewAnimator;
@@ -72,45 +66,6 @@ public class SessionActivity extends Activity {
 			}
 		} finally {
 			database.close();
-		}
-	}
-
-	private static class SessionAdapter extends BaseAdapter {
-		private final Context context;
-		private final List<Session> sessions;
-
-		public SessionAdapter(Context context, List<Session> sessions) {
-			super();
-			this.context = context;
-			this.sessions = sessions;
-		}
-
-		@Override
-		public int getCount() {
-			return sessions.size();
-		}
-
-		@Override
-		public Session getItem(int position) {
-			return sessions.get(position);
-		}
-
-		@Override
-		public long getItemId(int position) {
-			return position;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			Session session = getItem(position);
-			View ret = getLayoutInflater().inflate(R.layout.sessions_item, parent, false);
-			TextView title = (TextView) ret.findViewById(R.id.title);
-			title.setText(session.title);
-			return ret;
-		}
-
-		private LayoutInflater getLayoutInflater() {
-			return (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		}
 	}
 }
--- AgileDayConferenceApp/src/it/agileday/ui/sessions/SessionAdapter.java
+++ AgileDayConferenceApp/src/it/agileday/ui/sessions/SessionAdapter.java
@@ -1,19 +1,3 @@
-/*
-   Copyright 2010 Ilias Bartolini, Andrea Chiarini, Luigi Bozzo
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
-
 package it.agileday.ui.sessions;
 
 import it.agileday.R;
@@ -33,6 +17,7 @@ public class SessionAdapter extends BaseAdapter {
 	private final List<Session> sessions;
 
 	public SessionAdapter(Context context, List<Session> sessions) {
+		super();
 		this.context = context;
 		this.sessions = sessions;
 	}
@@ -49,19 +34,19 @@ public class SessionAdapter extends BaseAdapter {
 
 	@Override
 	public long getItemId(int position) {
-		return sessions.get(position).id;
+		return position;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		Session session = getItem(position);
-		LayoutInflater layoutInflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View ret = (convertView == null ? layoutInflater.inflate(
-				R.layout.sessions_item, parent, false) : convertView);
-
-		TextView tv = (TextView) ret.findViewById(R.id.title);
-		tv.setText(session.title);
+		View ret = getLayoutInflater().inflate(R.layout.sessions_item, parent, false);
+		TextView title = (TextView) ret.findViewById(R.id.title);
+		title.setText(session.title);
 		return ret;
+	}
+
+	private LayoutInflater getLayoutInflater() {
+		return (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
 }
