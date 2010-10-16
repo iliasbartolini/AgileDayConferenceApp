b7de294b119959931e77a0e8e9b0e50bfc9ad1c8
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2010-10-16 12:35:36 +0200

    Aggiunti test per fissare il comportamento dei metodi Dates.difference*

--- AgileDayConferenceAppTest/src/it/agileday/utils/DatesTest.java
+++ AgileDayConferenceAppTest/src/it/agileday/utils/DatesTest.java
@@ -21,13 +21,27 @@ public class DatesTest extends TestCase {
 
 		assertTrue(Dates.equal(d1, d1));
 	}
-	
+
+	// Should reflect java.util.Comparator specification
+	public void test_should_returns_a_negative_integer_if_the_first_argument_is_less_than_the_second() {
+		Date d1 = Dates.newDate(2010, 1, 1, 10, 0, 0);
+		Date d2 = Dates.newDate(2010, 1, 1, 10, 0, 1);
+		assertTrue(Dates.differenceSeconds(d1, d2) < 0);
+	}
+
+	// Should reflect java.util.Comparator specification
+	public void test_should_returns_a_positive_integer_as_the_first_argument_is_greater_than_the_second() {
+		Date d1 = Dates.newDate(2010, 1, 1, 10, 0, 1);
+		Date d2 = Dates.newDate(2010, 1, 1, 10, 0, 0);
+		assertTrue(Dates.differenceSeconds(d1, d2) > 0);
+	}
+
 	public void test_string_conversion() {
 		Date d1 = Dates.newDate(2010, 12, 31, 10, 0, 0);
 		assertEquals("2010-12-31 10:00:00", Dates.toDbString(d1));
-		
+
 		assertTrue(Dates.equal(d1, Dates.fromDbString("2010-12-31 10:00:00")));
-		
+
 		try {
 			Dates.fromDbString("today");
 			fail();
@@ -35,7 +49,7 @@ public class DatesTest extends TestCase {
 			assertEquals("Cannot parse 'today' as date", e.getMessage());
 		}
 	}
-	
+
 	public void test_difference_minutes() {
 		Date d1 = Dates.newDate(2010, 1, 1, 10, 30, 0);
 		Date d2 = Dates.newDate(2010, 1, 1, 10, 0, 0);
@@ -50,7 +64,7 @@ public class DatesTest extends TestCase {
 		Date d3 = Dates.newDate(2010, 1, 1, 12, 30, 0);
 		assertEquals(0, Dates.differenceHours(d1, d3));
 	}
-	
+
 	public void test_difference_days() {
 		Date d1 = Dates.newDate(2010, 1, 1, 12, 30, 0);
 		Date d2 = Dates.newDate(2010, 1, 1, 10, 0, 0);
@@ -60,24 +74,24 @@ public class DatesTest extends TestCase {
 		assertEquals(8, Dates.differenceDays(d3, d1));
 	}
 
-	public void test_parse_twitter_dates(){
+	public void test_parse_twitter_dates() {
 		String sampleDate = "Sun, 10 Oct 2010 20:56:58 +0002";
 		try {
 			Date tweetDate = Dates.fromTweeterString(sampleDate);
 			Calendar tweetCalendar = Calendar.getInstance();
 			tweetCalendar.setTime(tweetDate);
-			assertEquals(2010-1900, tweetDate.getYear() );
-			assertEquals(Calendar.SUNDAY, tweetCalendar.get(Calendar.DAY_OF_WEEK) );
-			assertEquals(Calendar.OCTOBER, tweetCalendar.get(Calendar.MONTH ));
+			assertEquals(2010 - 1900, tweetDate.getYear());
+			assertEquals(Calendar.SUNDAY, tweetCalendar.get(Calendar.DAY_OF_WEEK));
+			assertEquals(Calendar.OCTOBER, tweetCalendar.get(Calendar.MONTH));
 			assertEquals(2, tweetCalendar.get(Calendar.ZONE_OFFSET));
 			assertEquals(20, tweetCalendar.get(Calendar.HOUR_OF_DAY));
-			assertEquals(56, tweetCalendar.get(Calendar.MINUTE ));
+			assertEquals(56, tweetCalendar.get(Calendar.MINUTE));
 		} catch (RuntimeException e) {
 			e.printStackTrace();
 			fail("Date Parse Error");
 		}
 	}
-	
+
 	public void test_smart_date_differences() {
 		Date d1 = Dates.newDate(2010, 1, 1, 10, 30, 0);
 		Date d2 = Dates.newDate(2010, 1, 3, 11, 30, 0);
