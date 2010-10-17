98146b121a2e896f5537f76d5d7b5b6e44447228
Ilias Bartolini <ilias.bartolini@gmail.com>
2010-10-18 00:50:00 +0200

    Aggiornato header copyright e reformat code vari

--- AgileDayConferenceApp/src/it/agileday/data/TweetList.java
+++ AgileDayConferenceApp/src/it/agileday/data/TweetList.java
@@ -1,13 +1,28 @@
+/*
+	Copyright 2010 Italian Agile Day
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
 package it.agileday.data;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class TweetList extends ArrayList<Tweet> implements List<Tweet>  {
+public class TweetList extends ArrayList<Tweet> implements List<Tweet> {
 	private static final long serialVersionUID = 1L;
 	private static TweetList empty = new TweetList(0);
-	
+
 	public TweetList(int length) {
 		super(length);
 	}
@@ -15,5 +30,5 @@ public class TweetList extends ArrayList<Tweet> implements List<Tweet>  {
 	public static TweetList Empty() {
 		return empty;
 	}
-	
+
 }
--- AgileDayConferenceApp/src/it/agileday/ui/About.java
+++ AgileDayConferenceApp/src/it/agileday/ui/About.java
@@ -1,3 +1,19 @@
+/*
+	Copyright 2010 Italian Agile Day
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
 package it.agileday.ui;
 
 import it.agileday.R;
@@ -6,17 +22,16 @@ import android.os.Bundle;
 import android.text.util.Linkify;
 import android.widget.TextView;
 
-
 public class About extends Activity {
-	    @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.about);
-	        
-	        TextView  licenceText = (TextView) findViewById(R.id.about_license);
-			Linkify.addLinks(licenceText, Linkify.WEB_URLS);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
 
-			TextView  contentText = (TextView) findViewById(R.id.about_content);
-			Linkify.addLinks(contentText, Linkify.WEB_URLS);
-	    }
+		TextView licenceText = (TextView) findViewById(R.id.about_license);
+		Linkify.addLinks(licenceText, Linkify.WEB_URLS);
+
+		TextView contentText = (TextView) findViewById(R.id.about_content);
+		Linkify.addLinks(contentText, Linkify.WEB_URLS);
+	}
 }
